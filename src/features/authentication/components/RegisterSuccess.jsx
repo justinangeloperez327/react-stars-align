45d986960be4d39d1f2c1d1c5f862dc83de19021
justin <justin@redhq.com.au>
2024-12-24 21:45:40 +0400

    Update padding in RegisterSuccess component for improved layout

--- src/features/authentication/components/RegisterSuccess.jsx
+++ src/features/authentication/components/RegisterSuccess.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const RegisterSuccess = () => {
     return (
         <div className="fade-up group block mb-2 px-6 md:px-0 transition duration-300 ease-in-out">
-            <div className="text-center py-10 bg-white rounded-md shadow-md">
+            <div className="text-center py-10 px-4 bg-white rounded-md shadow-md">
                 <h2 className="text-2xl font-semibold text-gray-700">Registration Successful</h2>
                 <p className="text-gray-500 mt-4">
                     You have successfully registered. Please go to login page.
